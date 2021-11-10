64ecbdc733feba8a8d8e624eaae95408c1967f25
Khairil <sangbima.net@gmail.com>
2021-11-10 09:38:24 +0800

    init

--- migrations/m211029_051407_create_setting_table.php
+++ migrations/m211029_051407_create_setting_table.php
@@ -18,6 +18,35 @@ class m211029_051407_create_setting_table extends Migration
         ]);
 
         $this->addPrimaryKey('PK-settings-key_setting', '{{%setting}}', 'key_setting');
+
+        $this->insert('{{%setting}}', [
+            'key_setting' => 'fullname',
+            'key_value' => 'Nama Lengkap'
+        ]);
+        $this->insert('{{%setting}}', [
+            'key_setting' => 'jabatan',
+            'key_value' => 'Jabatan Guru'
+        ]);
+        $this->insert('{{%setting}}', [
+            'key_setting' => 'kepala_madrasah',
+            'key_value' => 'Nama Kepala Madrasah'
+        ]);
+        $this->insert('{{%setting}}', [
+            'key_setting' => 'nip',
+            'key_value' => 'NIP Guru'
+        ]);
+        $this->insert('{{%setting}}', [
+            'key_setting' => 'nip_kepala_madrasah',
+            'key_value' => 'NIP Kepala Madrasah'
+        ]);
+        $this->insert('{{%setting}}', [
+            'key_setting' => 'unit_kerja',
+            'key_value' => 'Unit Kerja'
+        ]);
+        $this->insert('{{%setting}}', [
+            'key_setting' => 'unit_organisasi',
+            'key_value' => 'Unit Organisasi'
+        ]);
     }
 
     /**
